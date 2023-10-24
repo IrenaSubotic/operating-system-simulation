ceaded78d5a9412c6fc9ddcee9d04dc93a6a0d16
Ostoja9 <64746466+Ostoja9@users.noreply.github.com>
2023-10-25 01:03:04 +0200

    Add files via upload

--- ORS3_projekat/src/memory/RAM.java
+++ ORS3_projekat/src/memory/RAM.java
@@ -1,6 +1,25 @@
 package memory;
 
 public class RAM {
- private final int SIZE=1024;
- private int[] ram=new int[SIZE];
+ private static final int SIZE=1024;
+ private static int[] ram=new int[SIZE];
+ 
+public static void initialize() {
+	for(int i=0;i<SIZE;i++)
+		ram[i]=-1;
+}
+public static void print() {
+	System.out.println("*********************RAM*********************");
+	for(int i=0;i<SIZE;i++) {
+		if(i%16 == 0) {
+			System.out.println();
+			}
+		System.out.print(ram[i]+" ");
+	}
+}
+public static void main(String[] args) {
+	RAM ram=new RAM();
+	ram.initialize();
+	ram.print();
+}
 }

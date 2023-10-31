62a22024d9657519603fccc1bf87e954587de4b1
IrenaSubotic <141313005+IrenaSubotic@users.noreply.github.com>
2023-10-31 21:54:39 +0100

    Add files via upload

---
+++ ORS3_projekat/src/shell/Shell.java
@@ -0,0 +1,10 @@
+package shell;
+
+import fileSystem.FileSystem;
+
+public class Shell {
+      public static FileSystem tree;
+      //public static Memory memory;
+      //public static Disk disk;
+      
+      }

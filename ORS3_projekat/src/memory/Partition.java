1389c1f51ae66e405445896cdd3dfcf66f9df98a
Ostoja9 <64746466+Ostoja9@users.noreply.github.com>
2023-11-19 01:16:36 +0100

    Update Partition.java

--- ORS3_projekat/src/memory/Partition.java
+++ ORS3_projekat/src/memory/Partition.java
@@ -1,6 +1,52 @@
 package memory;
 
+import java.util.ArrayList;
+import kernel.Process;
+
 public class Partition {
  private int[] data;
  private int size;
+ private int used;
+ private Process process;
+ private int positionInMemory=-1;
+ private static ArrayList<Partition> partitions=new ArrayList<>();
+
+ 
+ public Partition(Tree tree,Process process) {
+	this.process=process;
+	this.used=process.getInstructions().size();
+	data=new int[used];
+	tree.insertNode(process);
+	Node n=tree.findGivenNodeByName(tree.root,process);
+	size=n.getSize();
+	partitions.add(this);
+ }
+
+public static Partition getPartitionByAddress(int address) {
+	 for(Partition partition: partitions) {
+		 if(partition.getPositionInMemory()==address)
+			 return partition;
+	 }
+	 return null;
+ }
+
+ public static Partition getPartitionByProcess(Process process) {
+	 for(Partition partition: partitions) {
+		 if(partition.getProcess().equals(process))
+			 return partition;
+	 }
+	 return null;
+ }
+ public int getPositionInMemory() {
+	 return positionInMemory;
+ }
+ public int[] getData() {
+	 return data;
+ }
+ public int getSize() {
+	 return size;
+ }
+ public Process getProcess() {
+	 return process;
+ }
 }

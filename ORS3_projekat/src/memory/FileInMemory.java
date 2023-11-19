92b565c2a208a27f3f65dbd8d753bc368f389f21
Ostoja9 <64746466+Ostoja9@users.noreply.github.com>
2023-11-19 01:15:14 +0100

    Add files via upload

---
+++ ORS3_projekat/src/memory/FileInMemory.java
@@ -0,0 +1,59 @@
+package memory;
+
+public class FileInMemory {
+
+	private String name;
+	private int size;
+	private byte[] content;
+	private Pointer start;
+	private int length;
+	
+	public FileInMemory(String name, byte[] content) {
+		this.name=name;
+		this.content=content;
+		this.size=content.length;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	public void setStart(Pointer start) {
+		this.start=start;
+	}
+	public Pointer getStart() {
+		return start;
+	}
+	public byte[] getContent() {
+		return content;
+	}
+	public byte[] certainPart(int number) {
+		int counter=0;
+		byte[] part=new byte[Block.getSize()];
+		for(int i=number*Block.getSize();i<this.size;i++) {
+			part[counter]=this.content[i];
+			if(counter==Block.getSize()-1)
+				break;
+			counter++;
+		}
+		while(counter<Block.getSize()-1) {
+			byte[] blank=" ".getBytes();
+			part[counter]=blank[0];
+			counter++;
+		}
+		return part;
+	}
+
+	public void setLength(int length) {
+		// TODO Auto-generated method stub
+		this.length=length;
+	}
+	public String getName() {
+		return this.name;
+	}
+
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return this.length;
+	}
+	
+}

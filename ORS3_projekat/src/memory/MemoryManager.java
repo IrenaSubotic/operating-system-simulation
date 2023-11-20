c73367401af37538b23f8c7130380f339f7ae6ea
Ostoja9 <64746466+Ostoja9@users.noreply.github.com>
2023-11-20 02:36:23 +0100

    Update MemoryManager.java

--- ORS3_projekat/src/memory/MemoryManager.java
+++ ORS3_projekat/src/memory/MemoryManager.java
@@ -1,5 +1,22 @@
 package memory;
 
-public class MemoryManager {
+import java.util.ArrayList;
 
+import kernel.Process;
+
+public class MemoryManager {
+public ArrayList<Partition> partitionsInRAM;
+public void loadPartiton() {	
+	}
+	public int loadProcess(Process process) {
+		// TODO Auto-generated method stub
+		Tree.insertNode(process);
+		Tree.fillLists(Tree.root);
+		Tree.loadInRAM(process);
+		Tree.list.clear();
+		Tree.nodesList.clear();
+		Tree.occupationList.clear();
+		return 0;
+	}
 }
+

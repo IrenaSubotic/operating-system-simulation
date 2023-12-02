f8c95d91fdc3df58348f1f41485e5e202c7ed013
Ostoja9 <64746466+Ostoja9@users.noreply.github.com>
2023-12-02 15:27:14 +0100

    Update MemoryManager.java

--- ORS3_projekat/src/memory/MemoryManager.java
+++ ORS3_projekat/src/memory/MemoryManager.java
@@ -2,10 +2,11 @@ package memory;
 
 import java.util.ArrayList;
 
+import asembler.Operations;
 import kernel.Process;
 
 public class MemoryManager {
-public ArrayList<Partition> partitionsInRAM;
+public static ArrayList<Partition> partitionsInRAM;
 public MemoryManager() {
 	RAM.initialize();
 	Partition.initialize();
@@ -16,15 +17,55 @@ public static void printMemory() {
 	Operations.printRegisters();
 	SecondaryMemory.print();
 }
-	public int loadProcess(Process process) {
+public void removeProcess(Process process) {
+	Tree.deleteNode(process);
+	removePartition(Partition.getPartitionByProcess(process));
+}
+public void removePartition(Partition partition) {
+	if(partitionsInRAM.contains(partition)) {
+		RAM.remove(partition.getPositionInMemory(), partition.getSize());
+		
+		partition.setPositionInMemory(-1);
+		partitionsInRAM.remove(partition);
+	}
+}
+public static int loadInRAM(Process process) {
+	   int sum=0;
+		int pos=Tree.position(process);
+		System.out.println(pos);
+		int size=Tree.list.get(pos).getSize();
+		int usage=Tree.occupationList.get(pos);
+		for(int i=0;i<pos;i++){
+		   sum+=Tree.list.get(i).getSize();
+		}
+		System.out.println(sum);
+		Partition partition=new Partition(process);
+		partition.setPositionInMemory(sum);
+	    partitionsInRAM.add(partition);
+		//Dodavanje particije u listu particija, dodavanje u listu particija u RAM-u
+		RAM.set(sum,partition.getData());
+		return sum;
+	}
+	public int load(Process process) {
 		// TODO Auto-generated method stub
 		Tree.insertNode(process);
 		Tree.fillLists(Tree.root);
-		Tree.loadInRAM(process);
+		int position=loadInRAM(process);
 		Tree.list.clear();
 		Tree.nodesList.clear();
 		Tree.occupationList.clear();
-		return 0;
+		return position;
+	}
+	
+	public int loadProcess(Process process) {
+	Partition partition=Partition.getPartitionByProcess(process);
+	if(!partitionsInRAM.contains(partition)) {
+		return load(process);
+	}
+	else {
+		return process.getStartAddress();
+	}
+	
 	}
 	public static void main(String[] args) {
 		String weirdWord="10000";
@@ -32,5 +73,3 @@ public static void printMemory() {
 		System.out.println(res);
 	}
 }
-
-
